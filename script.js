28eb597ed25fa729a59f8750a3080e4d14265a8c
BERTONERI Sabine (SNCF VOYAGEURS / Direction de Lignes TER Ouest Provence / EQUIPE MIXTE MIRAMAS) <7011142g@commun.ad.sncf.fr>
2022-10-28 08:44:15 +0200

    V1 without video

---
+++ script.js
@@ -0,0 +1,8 @@
+
+   const btn = document.getElementById("btn");
+   const img = document.querySelector(".img");
+   btn.addEventListener('click', () => {
+     img.classList.toggle('show')
+     alert('Et vous de quoi êtes vous coupable ?')
+   }) 
+     
